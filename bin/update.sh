4dd3eac9c9a590e238cf5515a032830b341627d1
mamemomonga <mamemomonga@gmail.com>
2018-08-17 21:56:26 +0900

    fix update.sh

--- bin/update.sh
+++ bin/update.sh
@@ -2,9 +2,12 @@
 set -eu
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd .. && pwd )"
 
+BRANCH=${1:-master}
+
 set -x
 cd $BASEDIR
-git pull origin master
+git fetch origin $BRANCH
+git checkout $BRANCH
 docker-compose build
 docker-compose down
 docker-compose up -d
